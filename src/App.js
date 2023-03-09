1c876b9e1680ac71642abcd307e2c3e531646ed5
Marcus Paeschke <marcus@mphasize.de>
2023-03-09 10:52:52 +0100

    Proof of concept
    - SVG Animations using SMIL
    - React Portals to replace/add content outside of the React container

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { createPortal } from "react-dom";
+import Underlined from "./components/Underlined";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const div1 = document.getElementById("portal1");
+  const div2 = document.getElementById("portal2");
+
+  const spans = document.getElementsByClassName("squiggleUnderlined");
+
+  let result = [];
+  if (div1) {
+    result.push(createPortal(<Underlined width={219} stroke="#FBCA04" />, div1));
+  }
+
+  if (div2) {
+    result.push(createPortal(<Underlined width={360} />, div2));
+  }
+
+  for (let span of spans) {
+    const boundingRect = span.getBoundingClientRect();
+    const style = {
+      top: `${boundingRect.bottom}px`,
+      left: `${boundingRect.left}px`,
+    };
+    const width = boundingRect.right - boundingRect.left;
+    const height = boundingRect.bottom - boundingRect.top;
+    result.push(createPortal(<Underlined width={width} style={style} />, span));
+  }
+
+  return result;
 }
 
 export default App;
