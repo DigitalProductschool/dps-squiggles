e43932968cf2f8960585790b39b1fed1951895db
Marcus Paeschke <marcus@mphasize.de>
2023-03-10 12:42:32 +0100

    Use parent-relative offsets and run after document ready

--- src/index.js
+++ src/index.js
@@ -2,23 +2,25 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Underlined from "./components/Underlined";
 
-const squiggles = document.querySelectorAll(".dps-squiggle");
+function addSquiggles() {
+  const squiggles = document.querySelectorAll(".dps-squiggle");
 
-squiggles.forEach((span) => {
-  const boundingRect = span.getBoundingClientRect();
-  const style = {
-    position: "absolute",
-    top: `${boundingRect.bottom}px`,
-    left: `${boundingRect.left}px`,
-  };
-  const width = boundingRect.right - boundingRect.left;
-  //const height = boundingRect.bottom - boundingRect.top;
+  squiggles.forEach((span) => {
+    const style = {
+      position: "absolute",
+      top: `${span.offsetTop+span.offsetHeight}px`,
+      left: `${span.offsetLeft}px`,
+    };
+    const width = span.offsetWidth;
 
-  const root = createRoot(span);
-  root.render(
-    <>
-      {span.innerHTML}
-      <Underlined width={width} style={style} />
-    </>
-  );
-});
+    const root = createRoot(span);
+    root.render(
+      <>
+        {span.innerHTML}
+        <Underlined width={width} style={style} />
+      </>
+    );
+  });
+}
+
+document.addEventListener("DOMContentLoaded", addSquiggles, false);
